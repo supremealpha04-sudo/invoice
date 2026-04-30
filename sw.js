501d30372a7bd599cc74f1d863d8e98097e2ae44
supremealpha04-sudo <supremealpha04@gmail.com>
2026-04-29 20:41:50 -0700

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'supremeinvoice-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/login.html',
+    '/manifest.json',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        ))
+    );
+});
